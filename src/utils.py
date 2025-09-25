d4c0afa8413f3c80ba1a0e29f091c93b940ca990
Girish Sawant <rishkddude37@gmail.com>
2025-09-26 00:32:19 +0530

    Added templates and app.py code

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path, obj):
     try:
@@ -18,13 +20,25 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try: 
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)  ### training model
+            param = params[list(models.keys())[i]]
+
+            #---Grid-SearchCV 
+            cv = GridSearchCV(
+                model,
+                param_grid=param,
+                cv=3
+            )
+            cv.fit(X_train, y_train)
+            model.set_params(**cv.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  ### training model
             y_train_preds = model.predict(X_train)
             y_test_preds = model.predict(X_test)
 
@@ -35,5 +49,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             return report
         
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
